105928fb11d138ab7daca4d19d72579dd5e53d95
Tanpatsu <tantantanpa2@gmail.com>
2020-09-16 02:01:20 +0900

    Add Python練習用ソースその2追加
    practice2.py 練習用ソースその2を新規追加

---
+++ practice2.py
@@ -0,0 +1,33 @@
+# Pythonのお勉強 問題集
+# http://python.rdy.jp/wiki.cgi?page=%CC%E4%C2%EA%BD%B8
+
+# simple
+# Hello, world!を表示
+print('Hello, world!')
+
+
+# 1から50までの和を計算して表示
+s = 0
+
+for x in list(range(1, 51)):
+    s += x
+print(s)
+
+
+# 2つの自然数の最大公約数を求める
+a,b = input('aの値を入力:'), input('bの値を入力')
+
+# 入力値を数値型に変換
+a,b = int(a),int(b)
+
+#再帰関数
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+print('値a(' + str(a) + ')と値b(' + str(b) + ')の最大公約数は' + str(gcd(a, b)) + 'です')
+
+
+# 2つの自然数の最小公倍数を求める
+print('値a(' + str(a) + ')と値b(' + str(b) + ')の最小公倍数は' + str(a * b // gcd(a, b)) + 'です')
