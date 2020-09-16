b78b489e5af2103946b59c82c7e483a6332795ed
Tanpatsu <tantantanpa2@gmail.com>
2020-09-17 00:37:02 +0900

    Add Pytjom練習用ソースその3追加
    practice3.py 練習用ソースその3を新規作成

---
+++ practice3.py
@@ -0,0 +1,78 @@
+# Tech-Joho.info「Python問題集」
+# https://tech-joho.info/python%E5%95%8F%E9%A1%8C%E9%9B%86/
+
+# 引き算
+# 10 – 2を計算し、変数answer_1に結果を代入せよ。
+answer_1 = 10 - 2
+print(answer_1)
+
+
+# 割り算
+# 100割る3を計算し、変数answer_2に結果を代入せよ。
+answer_2 = 100 / 3
+print(answer_2)
+
+
+# 掛け算
+# 100掛ける256を計算し、変数answer_3に結果を代入せよ。
+answer_3 = 100 * 256
+print(answer_3)
+
+
+# 割り算のあまり
+# 233 割る 50の余りを計算するコードを書いてください。
+answer_4 = 233 % 50
+print(answer_4)
+
+
+# 変数への代入の練習問題(1)
+# 変数answer_5に、文字列’tanuki’を代入せよ
+answer_5 = 'tanuki'
+print(answer_5)
+
+
+# 変数への代入の練習問題(2)
+# 1 + 3をPythonを使って計算し、変数answer_6に結果を代入せよ。
+answer_6 = 1 + 3
+print(answer_6)
+
+
+# 足した結果を足された変数に代入する(複合代入演算子)
+# 変数answer_7に、answer_7に220を足した値を代入するコードを書いてください。
+# ただし、計算式にはanswer_7を1度しか書いてはいけません。
+answer_7 = 0
+answer_7 += 220
+print(answer_7)
+
+
+# 引き算の結果を引かれた変数に代入する
+# 下記のコードのtargetというコメントの付いた行を演算子を1つしか使わない行に書き換えて、現状と同じことを実現してください
+a = 1024
+a = a - 256  # target
+print(a)
+
+answer_8 = 1024
+answer_8 -= 256
+print(answer_8)
+
+
+# 掛け算の結果を割られた変数に代入する
+# 掛け算をしている行を、複合代入演算子を使って書き換えて下さい。
+b = 906
+b = b * 216  # target
+print(b)
+
+answer_9 = 906
+answer_9 *= 216
+print(answer_9)
+
+
+# 割り算の結果を割られた変数に代入する
+# 下のコードの割り算の行を、演算子を1つしか使わない様に書き換えてください。
+c = 337
+c = c / 3  # target
+print(c)
+
+answer_10 = 337
+answer_10 /= 3
+print(answer_10)
